c88b03543c4417bffcfbee74ab98c1a75fcc7808
renearias <rene_811@hotmail.com>
2016-05-10 16:23:01 -0500

    fix reparado

--- src/app/http/http.ts
+++ src/app/http/http.ts
@@ -7,6 +7,6 @@ export const contentHeaders = new Headers();
 contentHeaders.append('Content-Type', 'application/json');
 contentHeaders.append('Accept', 'application/json');
 
-//export const contentHeadersWithToken = new Headers();
-//contentHeaders.append('Authorization', 'Bearer '+token);
-//contentHeaders.append('Content-Type', 'application/json');
+export const contentHeadersWithToken = new Headers();
+contentHeadersWithToken.append('Authorization', 'Bearer '+token);
+contentHeadersWithToken.append('Content-Type', 'application/json');
