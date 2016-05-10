d078635addca78dfce2e8c2b9aed4648bef06704
renearias <rene_811@hotmail.com>
2016-05-10 10:49:52 -0500

    clientes fomulario

--- src/app/clients/clients.ts
+++ src/app/clients/clients.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {Widget} from '../core/widget/widget';
 
 @Component({
 	selector: 'clients',
 	template: require('./clients.html')
+        directives: [Widget]
 })
 export class ClientsPage {
 }
