f0b85ff1467caa555bbca7f6fc413e43e01d8df8
renearias <rene_811@hotmail.com>
2016-05-10 16:22:03 -0500

    fix que no permite loguearse se comentan las lineas que ocacioann el error se deben separar las clases

--- src/app/http/http.ts
+++ src/app/http/http.ts
@@ -5,7 +5,8 @@ let token = localStorage.getItem('jwt');
 
 export const contentHeaders = new Headers();
 contentHeaders.append('Content-Type', 'application/json');
+contentHeaders.append('Accept', 'application/json');
 
-export const contentHeadersWithToken = new Headers();
-contentHeaders.append('Authorization', 'Bearer '+token);
-contentHeaders.append('Content-Type', 'application/json');
+//export const contentHeadersWithToken = new Headers();
+//contentHeaders.append('Authorization', 'Bearer '+token);
+//contentHeaders.append('Content-Type', 'application/json');
