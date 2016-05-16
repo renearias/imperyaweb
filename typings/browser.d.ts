b83907d075624af500c16af0f70fd636b20d0e74
renearias <rene_811@hotmail.com>
2016-05-16 18:28:42 -0500

    typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,6 +1,6 @@
-/// <reference path="browser/ambient/angular-protractor/angular-protractor.d.ts" />
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/hammerjs/index.d.ts" />
+/// <reference path="browser/ambient/jquery/index.d.ts" />
 /// <reference path="browser/ambient/ng2/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
 /// <reference path="browser/ambient/selenium-webdriver/index.d.ts" />
