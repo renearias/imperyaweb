8ac1d8ef916f03d87eb71d1418ccfb3eb3d93af6
renearias <rene_811@hotmail.com>
2016-05-18 17:37:29 -0500

    fix

--- src/app/components/tables-dynamic/columnAction.ts
+++ src/app/components/tables-dynamic/columnAction.ts
@@ -20,9 +20,9 @@ export var columnAction = {
         actionsString += '<div class="wraper">'; ///start html
         var routeParameters, attributes, visibleFlag, roleFlag;
         
-        actionsString+='<a class="btn btn-info btn-xs glyphicon glyphicon-eye-open" href=""></a>'+
-                      '<a class="btn btn-warning btn-xs glyphicon glyphicon-pencil" href=""></a>'+
-                      '<a class="btn btn-danger btn-xs glyphicon glyphicon-trash" href=""></a>';
+        actionsString+='<a class="btn btn-info btn-xs glyphicon glyphicon-eye-open" href="#"></a>'+
+                      '<a class="btn btn-warning btn-xs glyphicon glyphicon-pencil" href="#"></a>'+
+                      '<a class="btn btn-danger btn-xs glyphicon glyphicon-trash" href="#"></a>';
         actionsString += '</div>'; ///endhtml
         return actionsString;
     },
