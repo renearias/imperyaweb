1fa191a2214658af852d073be4524502fa0cdf5a
abnersaavedra <abnersaavedra777@gmail.com>
2016-05-21 13:59:58 -0400

    Fixes de estandares en codificación ts,en columnAction.ts uso de comando npm run watch:prod

--- src/app/components/tables-dynamic/columnAction.ts
+++ src/app/components/tables-dynamic/columnAction.ts
@@ -3,27 +3,27 @@
  */
 
 export var columnAction = {
-    //"contentPadding": "{{ column.padding }}",
-    //"defaultContent": "{{ column.defaultContent }}",
-    "name": "Acciones",
-    "orderable": false,
-    "searchable": false,
-    "title": "Acciones",
-    ///"type": "{{ column.type }}",
-       // "visible": false,
-       // "className": "never {{ column.class }}",
-    //"width": "{{ column.width }}",
-    //"data": "{{ column.data }}",
-    "render": function(data, type, row, meta) {
+    //'contentPadding': '{{ column.padding }}',
+    //'defaultContent': '{{ column.defaultContent }}',
+    'name': 'Acciones',
+    'orderable': false,
+    'searchable': false,
+    'title': 'Acciones',
+    ///'type': '{{ column.type }}',
+       // 'visible': false,
+       // 'className': 'never {{ column.class }}',
+    //'width': '{{ column.width }}',
+    //'data': '{{ column.data }}',
+    'render': function(data, type, row, meta) {
 
-        var actionsString = "";
+        var actionsString = '';
         actionsString += '<div class="wraper">'; ///start html
         var routeParameters, attributes, visibleFlag, roleFlag;
         
-        actionsString+='<a class="btn btn-info btn-xs glyphicon glyphicon-eye-open" href="#"></a>'+
-                      '<a class="btn btn-warning btn-xs glyphicon glyphicon-pencil" href="#"></a>'+
+        actionsString += '<a class="btn btn-info btn-xs glyphicon glyphicon-eye-open" href="#"></a>' +
+                      '<a class="btn btn-warning btn-xs glyphicon glyphicon-pencil" href="#"></a>' +
                       '<a class="btn btn-danger btn-xs glyphicon glyphicon-trash" href="#"></a>';
         actionsString += '</div>'; ///endhtml
         return actionsString;
-    },
-}
+    }
+};
