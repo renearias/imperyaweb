578114eb8b10e4212576ef059c25a15566658d4d
abnersaavedra <abnersaavedra777@gmail.com>
2016-05-26 13:13:53 -0400

    Ingresos Services, comienzo con ingresos

---
+++ src/app/ingresos/ingresos.service.ts
@@ -0,0 +1,48 @@
+/* 
+ *
+ *Abner Saavedra
+ * 
+ */
+import {Request, Response} from '@angular/http';
+import {RESTClient, GET, PUT, POST, DELETE, BaseUrl, Headers, DefaultHeaders, Path, Body, Query} from '../rest/rest-client';
+
+import {Todo} from './models/Todo';
+import {SessionFactory} from './sessionFactory';
+
+@Injectable()
+@BaseUrl("http://imperya.arxis.la/api/")
+@DefaultHeaders({
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+})
+export class TodoIngresosRESTClient extends RESTClient {
+
+    protected requestInterceptor(req: Request) {
+        if (SessionFactory.getInstance().isAuthenticated) {
+            req.headers.append('jwt', SessionFactory.getInstance().credentials.jwt);
+        }
+    }
+
+    protected requestInterceptor(req: Response) {
+        // do sg with responses
+    }
+
+    @GET("todo/")
+    public getTodos( @Query("sort") sort?: string): Observable { return null; };
+
+    @GET("todo/{id}")
+    public getTodoById( @Path("id") id: string): Observable { return null; };
+
+    @POST("todo")
+    public postTodo( @Body todo: Todo): Observable { return null; };
+
+    @PUT("todo/{id}")
+    public putTodoById( @Path("id") id: string, @Body todo: Todo): Observable { return null; };
+
+    @DELETE("todo/{id}")
+    public deleteTodoById( @Path("id") id: string): Observable { return null; };
+
+}
+ 
+
+
