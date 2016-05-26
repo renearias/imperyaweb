dda595224053218e90bf1b8b39fcb4fb3da39d35
renearias <rene_811@hotmail.com>
2016-05-26 18:19:54 -0500

    rest client

--- src/app/productos/producto-rest.client.ts
+++ src/app/productos/producto-rest.client.ts
@@ -1,8 +1,8 @@
 import {Injectable} from "@angular/core";
 import {AuthHttp} from 'angular2-jwt';
 import {Observable} from "rxjs/Observable";
-import {Request, Response} from '@angular/http';
-import {RESTClient, GET, PUT, POST, DELETE, BaseUrl, Headers, DefaultHeaders, Path, Body, Query} from '../rest/rest-client';
+import {Response} from '@angular/http';
+import {RESTClient, GET, PUT, POST, DELETE, BaseUrl, DefaultHeaders, Path, Body, Query} from '../rest/rest-client';
 
 import {urlApi} from  '../http/http';
 import {Producto} from './producto';
@@ -31,18 +31,18 @@ export class ProductoRESTClient extends RESTClient {
     
 
     @GET("productos/")
-    public getProductos( @Query("sort") sort?: string): Observable<any> { return null; };
+    public getProductos( @Query("sort") sort?: string): Observable<Response> { return null; };
 
     @GET("productos/{id}")
     public getProductoById( @Path("id") id: number | string): Observable<Response> { return null; };
 
     @POST("productos")
-    public postProducto( @Body producto: Producto): Observable<any> { return null; };
+    public postProducto( @Body producto: Producto): Observable<Response> { return null; };
 
     @PUT("productos/{id}")
-    public putProductoById( @Path("id") id: number | string, @Body producto: Producto): Observable<any> { return null; };
+    public putProductoById( @Path("id") id: number | string, @Body producto: Producto): Observable<Response> { return null; };
 
     @DELETE("productos/{id}")
-    public deleteProductoById( @Path("id") id: number | string): Observable<any> { return null; };
+    public deleteProductoById( @Path("id") id: number | string): Observable<Response> { return null; };
 
 }
