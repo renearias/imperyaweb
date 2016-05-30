cb81bc2d4b3699e67729fd59016b2d5fb2bcd4ca
abnersaavedra <abnersaavedra777@gmail.com>
2016-05-30 16:33:29 -0400

    Fixes

--- src/app/ingresos/ingreso.ts
+++ src/app/ingresos/ingreso.ts
@@ -12,13 +12,16 @@ export class Ingreso {
     public forma_pago: FormasPagos;
     public id: number;
     public cliente: Cliente;
-    public fecha: any;
+    public id_cliente: number;
+    //public fecha: any;
     public monto: number;
     public descripcion: string;
     public referencia: string;
     public formapago: string;
    
-    constructor( r?:any, public fechaAlta?: any ) {
+    constructor( r?:any, public fecha?: any ) {
+        
+        this.id_cliente = this.cliente.id;
   
   if (typeof(r) != 'undefined')
       {
@@ -28,12 +31,12 @@ export class Ingreso {
             this[snakeToCamel(Object.keys(r)[i])] = r[Object.keys(r)[i]];
         }
       }
-      this.fechaAlta = moment().format('YYYY-MM-DDThh:mm');
+      this.fecha = moment().format('YYYY-MM-DDThh:mm');
     }
     
     public prepareToSend()
   {
-      this.fechaAlta=dateToApiDate(this.fechaAlta);
+      this.fecha=dateToApiDate(this.fecha);
       delete this['id'];
       delete this['atributos'];
   }
